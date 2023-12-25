5c8d8dc8fa52663236f4f49a75d61eec551a216d
Arham <arhamansari454647@gmail.com>
2023-12-25 11:16:11 +0500

    ASCII-TABLE

--- 02-First C++ Program/variables.cpp
+++ 02-First C++ Program/variables.cpp
@@ -1,6 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    cout<<"Hello World From: Arham :-)"<<endl;
-    cout<<"I just Started Learning C++"<<endl;
+
+// We use endl and '\n' for new line and to break the line.
+// endl and '\n' is basically used in place of enter
+
+// Data-Types
+// 1-> Int
+// 2-> Char
+// 3-> Bool
+// 4-> Float
+// 5-> Double
+
+int main()
+{
+    // => Data Types
+
+    int a = 123;
+    // cout << a << endl;
+
+    // char b = 'a';
+    // cout << b << endl;
+
+    // bool c = false;
+    // cout << c << endl;
+
+    // float f = 1.2;
+    // cout << f << endl;
+
+    // double d = 1.23;
+    // cout << d << endl;
+
+    cout <<sizeof(a)<<endl;
+
+    // Type Casting  =>Type Casting means that we assign the value of one data type to another
+    // 1 Byte = 8 Bits
+
+    // int a = 'a';
+    // cout << a << endl;
+    // char ch = 99;
+    // cout << ch << endl;
+    // return 0;
+
+
+
 }
+
+
