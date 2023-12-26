85d40261463d8ee7358ba19422b6bcee4e7f6a3f
Arham <arhamansari454647@gmail.com>
2023-12-26 12:38:57 +0500

    Lecture-04 Pattern Questions and Pdf's

---
+++ 04-Pattern Questions/patterns.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Pattern 1
+
+    // int n;
+    // cout << "Enter the number:";
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << j;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+    // return 0;
+
+    // Pattern 2
+
+    // int n;
+    // cout << "Enter the Number:";
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << n - j + 1;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+    // return 0;
+
+    // Pattern 3
+
+    // int n;
+    // cout << "Enter the Number:";
+    // cin >> n;
+    // int i = 1;
+    // int count = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << count;
+    //         count = count + 1;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // Pattern 4
+
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int i = 1;
+    // char star = '*';
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << star;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // Pattern 5
+
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << i;
+    //         j = j + 1;
+    //     }
+    //     cout << endl;
+    //     i = i + 1;
+    // }
+
+    // Pattern 6
+
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int row = 1;
+    // int count = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << count;
+    //         count = count + 1;
+    //         col = col + 1;
+    //     }
+    //     cout << endl;
+    //     row = row + 1;
+    // }
+
+    // Pattern 7
+
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int row = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     int count = row;
+    //     while (col <= row)
+    //     {
+    //         cout << count;
+    //         count = count + 1;
+    //         col = col + 1;
+    //     }
+    //     cout << endl;
+    //     row = row + 1;
+    // }
+
+    // Pattern 8
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int row = 1;
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << row - col + 1;
+    //         col = col + 1;
+    //     }
+    //     cout << endl;
+    //     row = row + 1;
+    // }
+
+    // Pattern 9
+}
