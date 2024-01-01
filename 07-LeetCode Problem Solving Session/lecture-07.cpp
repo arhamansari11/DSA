0fb836678b654c834ab4d51bb41016c910bbe5e7
Arham <arhamansari454647@gmail.com>
2024-01-01 12:12:48 +0500

    Lecture-07 LeetCode Problem Solving Session

---
+++ 07-LeetCode Problem Solving Session/lecture-07.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Reverse
+    // int n;
+    // cout << "Enter the Number : ";
+    // cin >> n;
+    // int ans = 0;
+    // while (n != 0)
+    // {
+    //     int digit = n % 10;
+    //     ans = (ans * 10) + digit;
+    //     n = n / 10;
+    // }
+    // cout << ans;
+
+    // Question-1
+
+    // class Solution
+    // {
+    // public:
+    //     int reverse(int x)
+    //     {
+    //         int ans = 0;
+    //         while (x != 0)
+    //         {
+    //             int digit = x % 10;
+    //             if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10))
+    //             {
+    //                 return 0;
+    //             }
+    //             ans = (ans * 10) + digit;
+    //             x = x / 10;
+    //         }
+    //         return ans;
+    //     }
+    // };
+
+    // Question-2
+
+    //     class Solution {
+    // public:
+    //     bool isPowerOfTwo(int n) {
+    //         for(int i = 0; i <=  30; i++){
+    //             int ans = pow (2 , i);
+    //             if(ans == n){
+    //                 return true;
+    //             }
+    //         }
+    //             return false;
+    //     }
+    // };
+
+    // Question-3
+}
