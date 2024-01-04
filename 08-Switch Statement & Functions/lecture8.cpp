30e799e1a0ab92ba17acf92d3915d76356fc3615
Arham <arhamansari454647@gmail.com>
2024-01-04 15:14:10 +0500

    Lecture-08 Switch Statement & Functions and Pdf's

--- 08-Switch Statement & Functions/lecture8.cpp
+++ 08-Switch Statement & Functions/lecture8.cpp
@@ -1,47 +1,179 @@
-#include <iostream>
+// #include <iostream>
+// #include <cstdlib>
+// using namespace std;
+// int main()
+// {
+//     Switch Statement
+
+//     int num;
+//     cout << "Enter The Number : ";
+//     cin >> num;
+//     char ch = 'a';
+//     switch (num)
+//     {
+//     case 1:
+//         switch (ch)
+//         {
+//         case 'a':
+//             cout << "It is nested Switch in case 1 ";
+//             break;
+
+//         default:
+//             break;
+//         }
+//         break;
+//     case 2:
+//         cout << " Tueday ";
+//         break;
+//     case 3:
+//         cout << " Wednesday ";
+//         break;
+//     case 4:
+//         cout << " Thursday ";
+//         break;
+//     case 5:
+//         cout << " Friday ";
+//         break;
+//     case 6:
+//         cout << " Saturday ";
+//         break;
+//     case 7:
+//         cout << " Sunday ";
+//         break;
+
+//     default:
+//         cout << "Your number is greater than 7 and less than 0 ";
+//         break;
+//     }
+
+//     while (1)
+//     {
+//         switch (1)
+//         {
+//         case 1:
+//             cout << "I break the Infinite Loop ";
+
+//             // Your condition to exit the loop
+//             if (1)
+//             {
+//                 exit(0); // Exit the program
+//             }
+
+//             break;
+
+//         default:
+//             // Code for default case
+//             break;
+//         }
+
+//         // Additional code if needed
+//     }
+
+//     return 0;
+
+//     Calculator using Switch Statement
+
+//     int num1, num2;
+//     char opt;
+//     cout << "Enter 1st Number : ";
+//     cin >> num1;
+//     cout << "Enter the 2nd Number : ";
+//     cin >> num2;
+//     cout << "Enter the Operator : ";
+//     cin >> opt;
+//     switch (opt)
+//     {
+//     case '+':
+//         cout << num1 + num2;
+//         break;
+//     case '-':
+//         cout << num1 - num2;
+//         break;
+//     case '*':
+//         cout << num1 * num2;
+//         break;
+//     case '/':
+//         cout << num1 / num2;
+//         break;
+//     case '%':
+//         cout << num1 % num2;
+//         break;
+
+//     default:
+//         cout << "Why my Calculator is not Working? ";
+//         break;
+//     }
+
+// }
+
+// Functions   =>  We use the function so we may use the code repeatidly
+
+// #include <iostream>
+// using namespace std;
+
+//  Function of Power
+
+// int power()
+// {
+//     int a;
+//     cout << "Ente the Value of a ";
+//     cin >> a;
+//     int b;
+//     cout << "Ente the Value of b ";
+//     cin >> b;
+//     int ans = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//         ans = ans * a;
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+
+//     int ans = power();
+//     cout << ans << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// Function of Even or Odd
+
+// bool isEven(int num1)
+// {
+//     if (num1 & 1)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return 1;
+//     }
+// }
+
+// int main()
+// {
+//     int num;
+//     cout << "Enter the number : ";
+//     cin >> num;
+//     if (isEven(num))
+//     {
+//         cout << "Number is Even ";
+//     }
+//     else
+//     {
+//         cout << "Number is Odd ";
+//     }
+// }
+
+// Funtion of Factorial
+
+#include<iostream>
 using namespace std;
-int main()
-{
-    // Switch Statement & Functions
 
-    int num;
-    cout << "Enter The Number : ";
-    cin >> num;
-    char ch = 'a';
-    switch (num)
-    {
-    case 1:
-        switch (ch)
-        {
-        case 'a':
-            cout << "It is nested Switch in case 1 ";
-            break;
 
-        default:
-            break;
-        }
-        break;
-    case 2:
-        cout << " Tueday ";
-        break;
-    case 3:
-        cout << " Wednesday ";
-        break;
-    case 4:
-        cout << " Thursday ";
-        break;
-    case 5:
-        cout << " Friday ";
-        break;
-    case 6:
-        cout << " Saturday ";
-        break;
-    case 7:
-        cout << " Sunday ";
-        break;
+int main (){
 
-    default:
-        cout << "Your number is greater than 7 and less than 0 ";
-        break;
-    }
 }
